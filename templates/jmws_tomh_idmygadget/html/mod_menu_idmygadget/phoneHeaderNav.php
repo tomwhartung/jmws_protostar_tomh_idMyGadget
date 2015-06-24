694717919fa5f4ba47f63e1fa61d144aef4a51be
Tom W Hartung <tomwhartung@gmail.com>
2015-06-24 14:56:02 -0600

    Fixed indentation issues.

--- templates/jmws_tomh_idmygadget/html/mod_menu_idmygadget/phoneHeaderNav.php
+++ templates/jmws_tomh_idmygadget/html/mod_menu_idmygadget/phoneHeaderNav.php
@@ -15,97 +15,98 @@ global $jmwsIdMyGadget;
 <?php // The menu class is deprecated. Use nav instead. ?>
 <?php if ( $jmwsIdMyGadget->getGadgetString() === $jmwsIdMyGadget::GADGET_STRING_PHONE ) : ?>
 	<div data-role="navbar">
-	<ul class="nav menu<?php echo $class_sfx;?>"<?php
-		$tag = '';
-	
-		if ($params->get('tag_id') != null)
+		<ul class="nav menu<?php echo $class_sfx;?>"<?php
+			$tag = '';
+
+			if ($params->get('tag_id') != null)
+			{
+				$tag = $params->get('tag_id') . '';
+				echo ' id="' . $tag . '"';
+			}
+		?>>
+		<?php
+		foreach ($list as $i => &$item)
 		{
-			$tag = $params->get('tag_id') . '';
-			echo ' id="' . $tag . '"';
-		}
-	?>>
-	<?php
-	foreach ($list as $i => &$item)
-	{
-		$class = 'item-' . $item->id;
-	
-		if (($item->id == $active_id) OR ($item->type == 'alias' AND $item->params->get('aliasoptions') == $active_id))
-		{
-			$class .= ' current';
-		}
-	
-		if (in_array($item->id, $path))
-		{
-			$class .= ' active';
-		}
-		elseif ($item->type == 'alias')
-		{
-			$aliasToId = $item->params->get('aliasoptions');
-	
-			if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
+			$class = 'item-' . $item->id;
+
+			if (($item->id == $active_id) OR ($item->type == 'alias' AND $item->params->get('aliasoptions') == $active_id))
+			{
+				$class .= ' current';
+			}
+
+			if (in_array($item->id, $path))
 			{
 				$class .= ' active';
 			}
-			elseif (in_array($aliasToId, $path))
+			elseif ($item->type == 'alias')
 			{
-				$class .= ' alias-parent-active';
+				$aliasToId = $item->params->get('aliasoptions');
+
+				if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
+				{
+					$class .= ' active';
+				}
+				elseif (in_array($aliasToId, $path))
+				{
+					$class .= ' alias-parent-active';
+				}
+			}
+
+			if ($item->type == 'separator')
+			{
+				$class .= ' divider';
+			}
+
+			if ($item->deeper)
+			{
+				$class .= ' deeper';
+			}
+
+			if ($item->parent)
+			{
+				$class .= ' parent';
+			}
+
+			if (!empty($class))
+			{
+				$class = ' class="' . trim($class) . '"';
+			}
+
+			echo '<li' . $class . '>';
+
+			// Render the menu item.
+			switch ($item->type) :
+				case 'separator':
+				case 'url':
+				case 'component':
+				case 'heading':
+					require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
+					break;
+
+				default:
+					require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
+					break;
+			endswitch;
+
+			// The next item is deeper.
+			if ($item->deeper)
+			{
+				echo '<ul class="nav-child unstyled small">';
+			}
+			elseif ($item->shallower)
+			{
+				// The next item is shallower.
+				echo '</li>';
+				echo str_repeat('</ul></li>', $item->level_diff);
+			}
+			else
+			{
+				// The next item is on the same level.
+				echo '</li>';
 			}
 		}
-	
-		if ($item->type == 'separator')
-		{
-			$class .= ' divider';
-		}
-	
-		if ($item->deeper)
-		{
-			$class .= ' deeper';
-		}
-	
-		if ($item->parent)
-		{
-			$class .= ' parent';
-		}
-	
-		if (!empty($class))
-		{
-			$class = ' class="' . trim($class) . '"';
-		}
-	
-		echo '<li' . $class . '>';
-	
-		// Render the menu item.
-		switch ($item->type) :
-			case 'separator':
-			case 'url':
-			case 'component':
-			case 'heading':
-				require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
-				break;
-	
-			default:
-				require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
-				break;
-		endswitch;
-	
-		// The next item is deeper.
-		if ($item->deeper)
-		{
-			echo '<ul class="nav-child unstyled small">';
-		}
-		elseif ($item->shallower)
-		{
-			// The next item is shallower.
-			echo '</li>';
-			echo str_repeat('</ul></li>', $item->level_diff);
-		}
-		else
-		{
-			// The next item is on the same level.
-			echo '</li>';
-		}
-	}
-	?></ul>
+		?>
+		</ul>
 	</div>
 <?php else : ?>
 	<p>No menu here on desktops and tablets</p>

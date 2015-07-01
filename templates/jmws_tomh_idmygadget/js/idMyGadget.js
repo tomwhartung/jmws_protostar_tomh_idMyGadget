bc78ad768cd7acb0e5be1dad57506da9af2f3a56
Tom W Hartung <tomwhartung@gmail.com>
2015-07-01 17:23:53 -0600

    Added idMyGadget.js .

---
+++ templates/jmws_tomh_idmygadget/js/idMyGadget.js
@@ -0,0 +1,19 @@
+/**
+ * @package     jmws_tomh_idMyGadget
+ * @subpackage  Templates.protostar
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @since       3.2
+ */
+
+(function($)
+{
+	$(document).ready(function()
+	{
+		var phoneBurgerMenuLeft = document.getElementById( 'phone-burger-menu-left' );
+		var phoneBurgerMenuRight = document.getElementById( 'phone-burger-menu-right' );
+	//	console.log( 'The document is ready!' );
+		console.log( 'phoneBurgerMenuLeft: ' + phoneBurgerMenuLeft );
+		console.log( 'phoneBurgerMenuRight: ' + phoneBurgerMenuRight );
+	})
+})(jQuery);

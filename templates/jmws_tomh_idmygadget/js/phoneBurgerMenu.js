e0c337e279de3e437daabd900d81f385067b2f99
Tom W Hartung <tomwhartung@gmail.com>
2015-07-03 13:17:05 -0600

    First pass at drawing the menu.

--- templates/jmws_tomh_idmygadget/js/phoneBurgerMenu.js
+++ templates/jmws_tomh_idmygadget/js/phoneBurgerMenu.js
@@ -12,13 +12,44 @@ var phoneBurgerMenu = {};
 {
 	$(document).ready(function()
 	{
-		phoneBurgerMenu.leftElt = document.getElementById( 'phone-burger-menu-left' );
-		phoneBurgerMenu.rightElt = document.getElementById( 'phone-burger-menu-right' );
-		phoneBurgerMenu.leftColor = phoneBurgerMenuLeftColor;     // set in admin console
-		phoneBurgerMenu.rightColor = phoneBurgerMenuRightColor;   // set in admin console
-		console.log( 'phoneBurgerMenu.leftElt: ' + phoneBurgerMenu.leftElt );
-		console.log( 'phoneBurgerMenu.leftColor: ' + phoneBurgerMenu.leftColor );
-		console.log( 'phoneBurgerMenu.rightElt: ' + phoneBurgerMenu.rightElt );
-		console.log( 'phoneBurgerMenu.rightColor: ' + phoneBurgerMenu.rightColor );
+		phoneBurgerMenu.drawPhoneBurgerMenus();
 	})
 })(jQuery);
+
+phoneBurgerMenu.drawPhoneBurgerMenus = function () {
+	phoneBurgerMenu.leftElement = document.getElementById( 'phone-burger-menu-left' );
+	phoneBurgerMenu.rightElement = document.getElementById( 'phone-burger-menu-right' );
+
+	if ( typeof phoneBurgerMenuLeftColor !== 'undefined' ) {     // set in admin console
+		phoneBurgerMenu.drawMenu( phoneBurgerMenu.leftElement, phoneBurgerMenuLeftColor );
+	}
+
+	if ( typeof phoneBurgerMenuRightColor === 'undefined' ) {     // set in admin console
+		phoneBurgerMenu.drawMenu( phoneBurgerMenu.rightElement, phoneBurgerMenuRightColor );
+	}
+};
+
+phoneBurgerMenu.drawMenu = function ( canvasElement, menuColor ) {
+	if ( canvasElement === null ) {
+		console.log( 'phoneBurgerMenu.drawMenu error: passed-in canvasElement is null!' );
+		return;
+	}
+
+	var context = canvasElement.getContext( '2d' );
+	var canvasWidth = canvasElement.width;
+	var canvasHeight = canvasElement.height;
+	var barHeight = Math.round( canvasHeight / 7 );
+	var gapHeight = barHeight;   // for now...
+	var marginWidth = Math.round( canvasWidth / 7 );
+	var barWidth = Math.round( canvasWidth - (marginWidth * 2) );
+
+	console.log( 'canvasWidth: ' + canvasWidth );
+	console.log( 'canvasHeight: ' + canvasHeight );
+
+	context.fillStyle = menuColor;
+	context.fillRect( marginWidth, barHeight, barWidth, barHeight );
+	context.fillRect( marginWidth, barHeight * 3, barWidth, barHeight );
+	context.fillRect( marginWidth, barHeight * 5, barWidth, barHeight );
+
+};
+
